5e489643c7b30c3b8889c281f5443045c66d9958
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-07-10 23:37:36 -0400

    feat(favorites): add use case interfaces for adding, retrieving, and removing favorite properties

---
+++ src/RealEstateApp.Application/Interfaces/UseCases/Favorites/IRemoveFavoriteUseCase.cs
@@ -0,0 +1,6 @@
+using RealEstateApp.Application.Common.Interfaces;
+using RealEstateApp.Application.Dtos.Favorites.Requests;
+
+namespace RealEstateApp.Application.Interfaces.UseCases.Favorites;
+
+public interface IRemoveFavoriteUseCase : IUseCase<RemoveFavoriteRequest>;
