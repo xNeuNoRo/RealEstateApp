1aa9d0fa825b832f4816748f0a20dae5a0051a11
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-07-16 15:09:36 -0400

    feat(services): add application and use cases services to Program.cs

--- app/RealEstateApp.Api/Program.cs
+++ app/RealEstateApp.Api/Program.cs
@@ -1,13 +1,25 @@
 using Microsoft.Extensions.DependencyInjection;
+using RealEstateApp.Application;
 using RealEstateApp.Infrastructure.Identity;
 using RealEstateApp.Infrastructure.Persistence;
 using RealEstateApp.Infrastructure.Shared;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddApplication();
 builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddSharedInfrastructure(builder.Configuration);
 builder.Services.AddIdentityForWebApi(builder.Configuration);
+
+builder.Services.AddPropertyUseCases();
+builder.Services.AddOfferUseCases();
+builder.Services.AddFavoriteUseCases();
+builder.Services.AddChatUseCases();
+builder.Services.AddCatalogUseCases();
+builder.Services.AddClientUseCases();
+builder.Services.AddAuthUseCases();
+builder.Services.AddAdminUseCases();
+builder.Services.AddIdentityClientUseCases();
 
 var app = builder.Build();
 
