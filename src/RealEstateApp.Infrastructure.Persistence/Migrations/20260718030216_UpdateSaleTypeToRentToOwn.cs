d1679f660d1d8f10bec3da168344d17aac65ef00
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-07-17 23:03:25 -0400

    feat(api): update SaleTypeCode and corresponding database entries to rename 'Transfer' to 'RentToOwn' with updated description

---
+++ src/RealEstateApp.Infrastructure.Persistence/Migrations/20260718030216_UpdateSaleTypeToRentToOwn.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstateApp.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSaleTypeToRentToOwn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "SaleTypes",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Code", "Description", "Name" },
+                values: new object[] { "RentToOwn", "Alquiler con derecho a compra futura.", "Alquiler con opción a compra" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "SaleTypes",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Code", "Description", "Name" },
+                values: new object[] { "Transfer", "Cesion de contrato a terceros.", "Traspaso" });
+        }
+    }
+}
